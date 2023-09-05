1236965aaaaa122b54fd95329a9d6c7c620f71ca
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-09-05 16:48:24 -0700

    created initial home text animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,20 @@
 import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+import './index.scss';
 
 const Home = () => {
 
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br /> I'm</h1>
-        <img src={LogoTitle} alt="developer" />
+        <h1>Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          lobodan
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend developer</h2>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
       </div>
     </div>
   );
